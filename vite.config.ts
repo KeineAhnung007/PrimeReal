4f55eee4ce184f83ea4d086e7312ee11d654e8a3
KeineAhnung007 <138381743+KeineAhnung007@users.noreply.github.com>
2025-09-19 13:57:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    base: '/PrimeReal/',
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
